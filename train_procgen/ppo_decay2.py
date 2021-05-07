31ecaa4b7b048b4cc01dd89bc23515e048c86da3
Maxwell Chen <maxhchen@gmail.com>
2021-05-07 01:00:54 -0700

    git add . Update initial ent coef estimate w/ scheduler git add .

--- train_procgen/ppo_decay2.py
+++ train_procgen/ppo_decay2.py
@@ -88,9 +88,13 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=2
         from baselines.ppo2.model import Model
         model_fn = Model
 
+    logger.logkv("ENT COEF VALUE:", ent_coef.value)
+    
+    #################################################################################
     model = model_fn(policy=policy, ob_space=ob_space, ac_space=ac_space, nbatch_act=nenvs, nbatch_train=nbatch_train,
-                    nsteps=nsteps, ent_coef=ent_coef, vf_coef=vf_coef,
+                    nsteps=nsteps, ent_coef=ent_coef.value, vf_coef=vf_coef,
                     max_grad_norm=max_grad_norm, comm=comm, mpi_rank_weight=mpi_rank_weight)
+    #################################################################################
 
     if load_path is not None:
         model.load(load_path)
