e77cba700127944f843680f8c13821061375f208
Maxwell Chen <maxhchen@gmail.com>
2021-05-11 13:10:57 -0700

    modify ppo_adamW

--- train_procgen/ppo_adamw.py
+++ train_procgen/ppo_adamw.py
@@ -13,6 +13,22 @@ except ImportError:
     MPI = None
 from baselines.ppo2.runner import Runner
 
+def set_global_seeds(i):
+    try:
+        import MPI
+        rank = MPI.COMM_WORLD.Get_rank()
+    except ImportError:
+        rank = 0
+            
+    myseed = i  + 1000 * rank if i is not None else None
+    try:
+        import tensorflow as tf
+        tf.compat.v1.set_random_seed(myseed)
+    except ImportError:
+        pass
+    np.random.seed(myseed)
+    random.seed(myseed)
+
 def constfn(val):
     def f(_):
         return val
